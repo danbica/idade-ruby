60d3820e55a7ad0667824a02c69f98fbea7993f5
Daniel Lima <daniel@macluan.local>
2020-04-01 13:04:38 -0300

    Adicionado o arquivo em Ruby

---
+++ idade.rb
@@ -0,0 +1,19 @@
+#Será exibido as mensagem de boas vindas ao usuario de acordo com o nome na variavel.
+nome = "Daniel"
+boas_vindas = "Seja bem-vindo(a) #{nome}"
+puts boas_vindas
+
+#Executará se o individuo for maior que 18 anos.
+idade = 19
+nome = "Lucas"
+
+if idade > 18
+    puts nome
+end 
+
+#for numero in(1..10)
+    #puts "Numero: #{numero}"
+#end    
+
+
+
